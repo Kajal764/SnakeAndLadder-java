351031f406c4712fba4abe87e928708ded7cc842
kajal764 <kajaldw666@gmail.com>
2021-08-27 09:17:25 +0530

    Added: Welcome message

---
+++ src/main/java/com/blz/snakeladder/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.blz.snakeladder;
+
+public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to snake and ladder project");
+    }
+}

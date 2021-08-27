715434759784a55be2e204eefcfd717a1d352388
kajal764 <kajaldw666@gmail.com>
2021-08-27 09:30:53 +0530

    Added: Functionality to get Dice value

--- src/main/java/com/blz/snakeladder/SnakeAndLadder.java
+++ src/main/java/com/blz/snakeladder/SnakeAndLadder.java
@@ -5,6 +5,7 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder project");
         int startPosition = 0;
-        
+        int diceNumber = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+
     }
 }

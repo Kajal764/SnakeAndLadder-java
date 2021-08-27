fd773519b0aaf6b27ca6cf21664c601b69e77e2b
kajal764 <kajaldw666@gmail.com>
2021-08-27 09:24:04 +0530

    Added: Start position

--- src/main/java/com/blz/snakeladder/SnakeAndLadder.java
+++ src/main/java/com/blz/snakeladder/SnakeAndLadder.java
@@ -4,5 +4,7 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder project");
+        int startPosition = 0;
+        
     }
 }
